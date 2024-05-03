553c8b635c428a158db220193cf2ca5d057ef8b5
Gurumoorthy M <gurunaveen22@gmail.com>
2024-05-03 23:09:55 +0530

    search insertion related problems

--- Array/BS/BS-Pratice.js
+++ Array/BS/BS-Pratice.js
@@ -90,3 +90,149 @@ const target = 10;
 console.log(smallestIndex(arr, target));
 
 */
+
+// return starting/ending index, of target ele, if doesnt exists return {-1,-1}
+
+/*
+var searchRange = function(nums, target) {
+    
+    let n = nums.length
+    let ans= n;
+
+    function startPos(nums, target) {
+        let l = 0;
+    let r = n-1;
+            while(l<=r) {
+                let mid = Math.floor((l+r)/2);
+                if(nums[mid] >= target) {
+                    ans = mid;
+                    r = mid-1;
+                } else {
+                    l = mid+1;
+                }
+            }
+            return ans;
+    }
+
+    function endPos(nums, target) {
+        let l = 0;
+        let r = n-1;
+        while(l<=r) {
+            let mid = Math.floor((l+r)/2);
+            if(nums[mid]>target) {
+                ans = mid;
+                r = mid-1;
+            } else {
+                l = mid+1;
+            }
+        }
+        // console.log(nums[ans-1]);
+        if(nums[ans-1] !== target || ans === n) return -1;
+        return ans;
+    }
+
+
+    // lb - lowerbound
+    let lb = startPos(nums, target);
+    if(nums[lb]!== target || lb == n) {
+        return [-1, -1];
+    }
+     return [lb, endPos(nums, target)-1];
+    
+};
+
+const nums = [5, 7, 7, 8, 10];
+const target = 8;
+
+
+// empty input
+// const nums = [];
+// const target = 0;
+
+// no target ele
+// const target = 8;   // [-1, -1]
+
+// only 1 target ele
+// const target = 10;
+
+
+
+console.log(searchRange(nums, target));
+*/
+
+/*
+
+var searchInsert = function(nums, target) {
+
+    let n = nums.length;
+    let l=0;
+    let r = n-1;
+    let ans = -1;
+// adding target ele after n ele
+    if(target > nums[n-1]) return n;
+    while(l<=r) {
+        let mid = Math.floor((l+r)/2);
+        if(nums[mid] >= target) {
+            ans = mid;
+            r = mid-1;
+        } else {
+            l = mid+1;
+        }
+    }
+    return ans;
+};
+
+const nums = [1,3,5,6];
+const target = 7;
+
+*/
+
+// search in a rotated sorted array
+
+// arr = [7, 8, 1, 2, 3, 4, 5]
+
+// 1. after finding mid,
+// 2. try to find in sorted side of the array
+// 3. try to check if the target exists in given range, if not move to anoter side
+
+/*
+var search = function(arr, target) {
+
+    let l = 0;
+    let r = arr.length-1;
+
+    while(l<=r) {
+
+        let mid = Math.floor((l+r)/2);
+
+        if(arr[mid] != 1) console.log(l);
+        if(arr[l] === arr[mid] && arr[r] == arr[mid]) {
+            l++;
+            r--;
+        }
+        if(target === arr[mid]) return mid;
+
+// 2. try to find in sorted side of the array
+        if(arr[l] <= arr[mid]) {
+
+// 3. try to check if the target exists in given range, if not move to anoter side
+            
+            if(arr[l] <= target && arr[r] >= target) {
+                r = mid-1;
+            } else {
+                l = mid+1;
+            }
+        } else {
+            if(arr[mid] <= target && arr[r] >= target) {
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+    }
+    return -1;
+}
+
+*/
+
+
